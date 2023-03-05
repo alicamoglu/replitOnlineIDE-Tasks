f8ae0580cdfbb4813ce9c9669d14732c604a5bb4
Ali Camoglu <119692910+alicamoglu@users.noreply.github.com>
2023-03-05 15:11:38 +0000

    First Solution from online IDE Replit

--- Main.java
+++ Main.java
@@ -5,6 +5,10 @@ public class Main {
     //DO NOT TOUCH BELOW
     Scanner s = new Scanner(System.in);
     String word = s.next();
+        boolean startWithA = word.charAt(0)== 'a',
+                endsWithE = word.charAt(word.length()-1)=='e';
+        System.out.println("Starts with a: " + startWithA);
+        System.out.println("Ends with e: " + endsWithE);
 
     //WRITE YOUR CODE BELOW
 
